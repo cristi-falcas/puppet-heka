d061c6e3bd35eec87da29debf8e37d4958d46cd3
Alexander Lindquister <alelindq@cisco.com>
2016-02-03 22:42:54 +0100

    Moving service config code to config class

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,38 @@
 # configures heka
 class heka::config {
+  case $heka::service_provider {
+    'init': {
+      # File resource for /etc/init/heka.conf, the Upstart config file:
+      file { '/etc/init.d/heka':
+        ensure  => 'file',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        content => template("${module_name}/${heka::service_template}"),
+        notify  => Service['heka'],
+      }
+    }
+    'systemd': {
+      file { '/etc/init.d/heka':
+        ensure => 'absent',
+      } ~>
+      file { '/lib/systemd/system/heka.service':
+        ensure  => 'file',
+        force   => true,
+        content => template("${module_name}/${heka::service_template}"),
+        notify  => Service['heka'],
+      } ~>
+      exec { 'heka-systemd-reload':
+        command     => 'systemctl daemon-reload',
+        path        => ['/bin', '/usr/bin'],
+        refreshonly => true,
+      }
+    }
+    default: {
+      fail("Unknown service provider ${$heka::service_provider}")
+    }
+  }
+  
   file { '/etc/heka':
     ensure  => directory,
     recurse => true,
