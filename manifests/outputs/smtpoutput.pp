1fcdb7b70950e6ecb8486863732d8a1a44ace9ef
Cristian Falcas <cristi.falcas@gmail.com>
2016-02-09 12:32:40 +0200

    update smtpoutput for 0.10; add buffering support

--- manifests/outputs/smtpoutput.pp
+++ manifests/outputs/smtpoutput.pp
@@ -5,60 +5,78 @@
 #
 # $ensure::                      This is used to set the status of the config file: present or absent
 #
-# $message_matcher::             Boolean expression, when evaluated to true passes the message to the filter for processing.
-#                                Defaults to matching nothing
+### Common Output Parameters::   Check heka::outputs::tcpoutput for the description
 #
-# $message_signer::              The name of the message signer. If specified only messages with this signer are passed to the
-#                                filter for processing.
+### SMTP Output
 #
-# $ticker_interval::             Frequency (in seconds) that a timer event will be sent to the filter. Defaults to not sending timer
-#                                events.
+# $send_from::                   The email address of the sender.
+#                                Default: "heka@localhost.localdomain"
 #
-# $encoder::                     Encoder to be used by the output. This should refer to the name of an encoder plugin section that
-#                                is specified elsewhere in the TOML configuration.
-#                                Messages can be encoded using the specified encoder by calling the OutputRunner's Encode() method.
+# $send_to::                     An array of email addresses where the output will be sent to.
 #
-# $use_framing::                 Specifies whether or not Heka's Stream Framing should be applied to the binary data returned from
-#                                the OutputRunner's Encode() method.
+# $subject::                     Custom subject line of email.
+#                                Default: "Heka [SmtpOutput]"
 #
-# $can_exit::                    Whether or not this plugin can exit without causing Heka to shutdown. Defaults to false.
+# $host::                        SMTP host to send the email to.
+#                                Default: "127.0.0.1:25"
 #
-
+# $auth::                        SMTP authentication type: "none", "Plain", "CRAMMD5".
+#                                Default: "none"
+#
+# $user::                        SMTP user name
+#
+# $password::                    SMTP user password
+#
+# $send_interval::               Minimum time interval between each email, in seconds. First email in an interval goes out immediately,
+#                                subsequent messages in the same interval are concatenated and all sent when the interval expires.
+#                                Defaults to 0, meaning all emails are sent immediately.
+#
 define heka::outputs::smtpoutput (
-  $ensure          = 'present',
+  $ensure              = 'present',
   # Common Output Parameters
-  $message_matcher,
-  $message_signer  = undef,
-  $ticker_interval = 300,
-  $encoder         = 'ProtobufEncoder',
-  $use_framing     = undef,
-  $can_exit        = undef,
+  $message_matcher     = undef,
+  $message_signer      = undef,
+  $ticker_interval     = 5,
+  $encoder             = undef,
+  $use_framing         = undef,
+  $can_exit            = undef,
+  $use_buffering       = undef,
+  # Buffering
+  $max_file_size       = undef,
+  $max_buffer_size     = undef,
+  $full_action         = undef,
+  $cursor_update_count = undef,
   # SMTP Output
-  $send_from       = "heka@${::fqdn}",
-  $send_to         = "root@${::fqdn}",
-  $subject         = 'Heka [SmtpOutput]',
-  $host            = '127.0.0.1:25',
-  $auth            = 'none',
-  $user            = undef,
-  $password        = undef,
-  $send_interval   = 0,
+  $send_from           = "heka@${::fqdn}",
+  $send_to             = "root@${::fqdn}",
+  $subject             = 'Heka [SmtpOutput]',
+  $host                = '127.0.0.1:25',
+  $auth                = 'none',
+  $user                = undef,
+  $password            = undef,
+  $send_interval       = 0,
 ) {
   # Common Output Parameters
   if $message_matcher { validate_string($message_matcher) }
   if $message_signer { validate_string($message_signer) }
-  validate_integer($ticker_interval)
+  if $ticker_interval { validate_integer($ticker_interval) }
   if $encoder { validate_string($encoder) }
   if $use_framing { validate_bool($use_framing) }
   if $can_exit { validate_bool($can_exit) }
+  if $use_buffering { validate_bool($use_buffering) }
+  # Buffering
+  if $max_file_size { validate_integer($max_file_size) }
+  if $max_buffer_size { validate_integer($max_buffer_size) }
+  if $full_action { validate_re($full_action, '^(shutdown|drop|block)$') }
+  if $cursor_update_count { validate_integer($cursor_update_count) }
   # SMTP Output
   validate_string($send_from)
-#  validate_array($send_to)
   validate_string($subject)
   validate_string($host)
   validate_string($auth)
   if $user { validate_string($user) }
   if $password { validate_string($password) }
-  validate_integer($send_interval)
+  if $send_interval { validate_integer($send_interval) }
 
   $plugin_name = "smtpoutput_${name}"
   heka::snippet { $plugin_name:
