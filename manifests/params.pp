3f2caa1b99d140538acbf3599605741d73745195
Alexander Lindquister <alelindq@cisco.com>
2016-02-19 18:13:26 +0100

    Fail if OS not supported before accessing any facts

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,23 +1,5 @@
 # Default parameters for heka module
 class heka::params {
-  $package_ensure = 'installed'
-  $cpuprof = undef
-  $max_message_loops = 4
-  $max_process_inject = 1
-  $max_process_duration = 100000
-  $max_timer_inject = 10
-  $max_pack_idle = undef
-  $maxprocs = $::processorcount
-  $memprof = undef
-  $poolsize = 100
-  $plugin_chansize = 30
-  $base_dir = '/var/cache/hekad'
-  $share_dir = '/usr/share/heka'
-  $sample_denominator = 1000
-  $pid_file = undef
-  $hostname = undef
-  $max_message_size = 65536
-  $logrotate = true
 
   case $::operatingsystem {
     'RedHat', 'CentOS', 'Fedora', 'Scientific', 'OracleLinux': {
@@ -51,4 +33,26 @@ class heka::params {
       fail("${::operatingsystem} not supported")
     }
   }
+
+  $package_ensure = 'installed'
+  $service_ensure = 'running'
+  $service_enable = true
+  $cpuprof = undef
+  $max_message_loops = 4
+  $max_process_inject = 1
+  $max_process_duration = 100000
+  $max_timer_inject = 10
+  $max_pack_idle = undef
+  $maxprocs = $::processorcount
+  $memprof = undef
+  $poolsize = 100
+  $plugin_chansize = 30
+  $base_dir = '/var/cache/hekad'
+  $share_dir = '/usr/share/heka'
+  $sample_denominator = 1000
+  $pid_file = undef
+  $hostname = undef
+  $max_message_size = 65536
+  $logrotate = true
+
 }
