0cdc5f14eb5285f47c866b5793d920054184db0e
Cristian Falcas <cristi.falcas@gmail.com>
2016-02-22 17:45:44 +0200

    switch to heka::snippet

--- manifests/decoder/rsyslogdecoder.pp
+++ manifests/decoder/rsyslogdecoder.pp
@@ -3,55 +3,63 @@
 #
 # === Parameters:
 #
-# $preserve_data::                True if the sandbox global data should be preserved/restored on plugin shutdown/startup.
-#                                 When true this works in conjunction with a global Lua _PRESERVATION_VERSION variable which
-#                                 is examined during restoration; if the previous version does not match the current version
-#                                 the restoration will be aborted and the sandbox will start cleanly. _PRESERVATION_VERSION should
-#                                 be incremented any time an incompatible change is made to the global data schema. If no version
-#                                 is set the check will always succeed and a version of zero is assumed.
+# $ensure::                       This is used to set the status of the config file: present or absent
+#                                 Default: present
 #
-# $memory_limit::                 The number of bytes the sandbox is allowed to consume before being terminated (default 8MiB).
+### Check heka::decoder::sandboxdecoder for common parameters
 #
-# $instruction_limit::            The number of instructions the sandbox is allowed to execute during the
-#                                 process_message/timer_event functions before being terminated (default 1M).
-#
-# $output_limit::                 The number of bytes the sandbox output buffer can hold before being terminated (default 63KiB).
-#                                 Warning: messages exceeding 64KiB will generate an error and be discarded by the standard output
-#                                 plugins (File, TCP, UDP) since they exceed the maximum message size.
-#
-# $module_directory::             The directory where 'require' will attempt to load the external Lua modules from. Defaults to ${SHARE_DIR}/lua_modules.
-#
+### RsyslogDecoder Parameters
 # $hostname_keep::                Always preserve the original 'Hostname' field set by Logstreamer's 'hostname' configuration
-# setting.
+#                                 setting.
+#                                 Type: bool
 #
 # $template::                     The 'template' configuration string from rsyslog.conf.
 #                                 http://rsyslog-5-8-6-doc.neocities.org/rsyslog_conf_templates.html
+#                                 Type: string
 #
 # $tz::                           If your rsyslog timestamp field in the template does not carry zone offset information, you may
 #                                 set an offset to be applied to your events here. Typically this would be used with the
 #                                 "Traditional" rsyslog formats.
+#                                 Type: string
 #
-
 define heka::decoder::rsyslogdecoder (
+  $ensure            = 'present',
   # Common Sandbox Parameters
-  $preserve_data          = undef,
-  $memory_limit           = undef,
-  $instruction_limit      = undef,
-  $output_limit           = undef,
-  $module_directory       = undef,
-  # rsyslog Parameters
-  $type                   = $name,
-  $hostname_keep          = true,
-  $template               = '%TIMESTAMP% %HOSTNAME% %syslogtag%%msg:::sp-if-no-1st-sp%%msg:::drop-last-lf%\n',
-  $tz                     = 'UTC',
+  $preserve_data     = undef,
+  $memory_limit      = undef,
+  $instruction_limit = undef,
+  $output_limit      = undef,
+  $module_directory  = undef,
+  # RsyslogDecoder Parameters
+  $type              = $name,
+  $hostname_keep     = true,
+  $template          = '%TIMESTAMP% %HOSTNAME% %syslogtag%%msg:::sp-if-no-1st-sp%%msg:::drop-last-lf%\n',
+  $tz                = 'UTC',
 ) {
-  heka::decoder::sandboxdecoder { $name:
-    filename => 'lua_decoders/rsyslog.lua',
-    config   => {
-      'type'          => $type,
-      'hostname_keep' => $hostname_keep,
-      'template'      => $template,
-      'tz'            => $tz,
-    },
+  validate_re($ensure, '^(present|absent)$')
+  # Common Sandbox Parameters
+  if $preserve_data { validate_bool($preserve_data) }
+  if $memory_limit { validate_integer($memory_limit) }
+  if $instruction_limit { validate_integer($instruction_limit) }
+  if $output_limit { validate_integer($output_limit) }
+  if $module_directory { validate_string($module_directory) }
+  # RsyslogDecoder Parameters
+  validate_string($type, $template, $tz)
+  validate_bool($hostname_keep)
+
+  $sandbox_type = 'SandboxDecoder'
+  $script_type = 'lua'
+  $filename = 'lua_decoders/rsyslog.lua'
+  $config = {
+    'type'          => $type,
+    'hostname_keep' => $hostname_keep,
+    'template'      => $template,
+    'tz'            => $tz,
+  }
+
+  $full_name = "rsyslogdecoder_${name}"
+  heka::snippet { $full_name:
+    ensure  => $ensure,
+    content => template("${module_name}/sandbox.toml.erb"),
   }
 }
