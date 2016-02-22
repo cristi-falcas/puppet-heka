5d813d0538f1eafd84358358e2ac6004a9e8ebc3
Cristian Falcas <cristi.falcas@gmail.com>
2016-02-22 18:10:10 +0200

    move kafka plugins to new format

--- manifests/inputs/kafkainput.pp
+++ manifests/inputs/kafkainput.pp
@@ -2,99 +2,122 @@
 #
 # === Parameters:
 #
-# $ensure::                             This is used to set the status of the config file: present or absent
+# $ensure::                       This is used to set the status of the config file: present or absent
+#                                 Default: present
 #
-### Common Input Parameters::           Check heka::inputs::tcpinput for the description
+### Common Input Parameters::     Check heka::inputs::tcpinput for the description
 #
 ### Kafka Input Parameters
 #
-# $id::                                 Client ID string.
-#                                       Default is the hostname.
+# $id::                           Client ID string.
+#                                 Default is the hostname.
+#                                 Type: string
 #
-# $addrs::                              List of brokers addresses.
+# $addrs::                        List of brokers addresses.
+#                                 Type: []string
 #
-# $metadata_retries::                   How many times to retry a metadata request when a partition is in the middle of leader election.
-#                                       Default is 3.
+# $metadata_retries::             How many times to retry a metadata request when a partition is in the middle of leader election.
+#                                 Default is 3.
+#                                 Type: int
 #
-# $wait_for_election::                  How long to wait for leader election to finish between retries (in milliseconds).
-#                                       Default is 250.
+# $wait_for_election::            How long to wait for leader election to finish between retries (in milliseconds).
+#                                 Default is 250.
+#                                 Type: uint32
 #
-# $background_refresh_frequency::       How frequently the client will refresh the cluster metadata in the background (in milliseconds).
-#                                       Default is 600000 (10 minutes). Set to 0 to disable.
+# $background_refresh_frequency:: How frequently the client will refresh the cluster metadata in the background (in milliseconds).
+#                                 Default is 600000 (10 minutes). Set to 0 to disable.
+#                                 Type: uint32
 #
-# $max_open_reqests::                   How many outstanding requests the broker is allowed to have before blocking attempts to send.
-#                                       Default is 4.
+# $max_open_reqests::             How many outstanding requests the broker is allowed to have before blocking attempts to send.
+#                                 Default is 4.
+#                                 Type: int
 #
-# $dial_timeout::                       How long to wait for the initial connection to succeed before timing out and returning an error (in milliseconds).
-#                                       Default is 60000 (1 minute).
+# $dial_timeout::                 How long to wait for the initial connection to succeed before timing out and returning an error (in milliseconds).
+#                                 Default is 60000 (1 minute).
+#                                 Type: uint32
 #
-# $read_timeout::                       How long to wait for a response before timing out and returning an error (in milliseconds).
-#                                       Default is 60000 (1 minute).
+# $read_timeout::                 How long to wait for a response before timing out and returning an error (in milliseconds).
+#                                 Default is 60000 (1 minute).
+#                                 Type: uint32
 #
-# $write_timeout::                      How long to wait for a transmit to succeed before timing out and returning an error (in milliseconds).
-#                                       Default is 60000 (1 minute).
+# $write_timeout::                How long to wait for a transmit to succeed before timing out and returning an error (in milliseconds).
+#                                 Default is 60000 (1 minute).
+#                                 Type: uint32
 #
-# $topic::                              Kafka topic (must be set).
+# $topic::                        Kafka topic (must be set).
+#                                 Type: string
 #
-# $partition::                          Kafka topic partition.
-#                                       Default is 0.
+# $partition::                    Kafka topic partition.
+#                                 Default is 0.
+#                                 Type: int32
 #
-# $group::                              A string that uniquely identifies the group of consumer processes to which this consumer belongs.
-#                                       By setting the same group id multiple processes indicate that they are all part of the same consumer group.
-#                                       Default is the id.
+# $group::                        A string that uniquely identifies the group of consumer processes to which this consumer belongs.
+#                                 By setting the same group id multiple processes indicate that they are all part of the same consumer group.
+#                                 Default is the id.
+#                                 Type: string
 #
-# $default_fetch_size::                 The default (maximum) amount of data to fetch from the broker in each request.
-#                                       The default is 32768 bytes.
+# $default_fetch_size::           The default (maximum) amount of data to fetch from the broker in each request.
+#                                 The default is 32768 bytes.
+#                                 Type: int32
 #
-# $min_fetch_size::                     The minimum amount of data to fetch in a request - the broker will wait until at least this many bytes are available.
-#                                       The default is 1, as 0 causes the consumer to spin when no messages are available.
+# $min_fetch_size::               The minimum amount of data to fetch in a request - the broker will wait until at least this many bytes are available.
+#                                 The default is 1, as 0 causes the consumer to spin when no messages are available.
+#                                 Type: int32
 #
-# $max_message_size::                   The maximum permittable message size - messages larger than this will return MessageTooLarge.
-#                                       The default of 0 is treated as no limit.
+# $max_message_size::             The maximum permittable message size - messages larger than this will return MessageTooLarge.
+#                                 The default of 0 is treated as no limit.
+#                                 Type: int32
 #
-# $max_wait_time::                      The maximum amount of time the broker will wait for min_fetch_size bytes
-#                                       to become available before it returns fewer than that anyways.
-#                                       The default is 250ms, since 0 causes the consumer to spin when no events are available.
-#                                       100-500ms is a reasonable range for most cases.
-# $offset_method::                      The method used to determine at which offset to begin consuming messages. The valid values are:
-#                                        - "Manual" Heka will track the offset and resume from where it last left off (default).
-#                                        - "Newest" Heka will start reading from the most recent available offset.
-#                                        - "Oldest" Heka will start reading from the oldest available offset.
+# $max_wait_time::                The maximum amount of time the broker will wait for min_fetch_size bytes
+#                                 to become available before it returns fewer than that anyways.
+#                                 The default is 250ms, since 0 causes the consumer to spin when no events are available.
+#                                 100-500ms is a reasonable range for most cases.
+#                                 Type: uint32
 #
-# $event_buffer_size::                  The number of events to buffer in the Events channel. Having this non-zero permits the consumer to
-#                                       continue fetching messages in the background while client code consumes events,greatly improving throughput.
-#                                       The default is 16.
+# $offset_method::                The method used to determine at which offset to begin consuming messages. The valid values are:
+#                                  - "Manual" Heka will track the offset and resume from where it last left off (default).
+#                                  - "Newest" Heka will start reading from the most recent available offset.
+#                                  - "Oldest" Heka will start reading from the oldest available offset.
+#                                 Type: string
 #
-
-
+# $event_buffer_size::            The number of events to buffer in the Events channel. Having this non-zero permits the consumer to
+#                                 continue fetching messages in the background while client code consumes events,greatly improving throughput.
+#                                 The default is 16.
+#                                 Type: int
+#
 define heka::inputs::kafkainput (
-  $ensure                               = 'present',
+  $ensure                       = 'present',
   # Common Input Parameters
-  $splitter                             = 'TokenSplitter',
-  $decoder                              = undef,
-  $synchronous_decode                   = undef,
-  $send_decode_failures                 = undef,
-  $can_exit                             = undef,
+  $splitter                     = 'TokenSplitter',
+  $decoder                      = undef,
+  $synchronous_decode           = undef,
+  $send_decode_failures         = undef,
+  $can_exit                     = undef,
   # Kafka Input
-  $id                                   = undef,
+  $id                           = undef,
+  # lint:ignore:parameter_order
   $addrs,
-  $metadata_retries                     = undef,
-  $wait_for_election                    = undef,
-  $background_refresh_frequency         = undef,
-  $max_open_reqests                     = undef,
-  $dial_timeout                         = undef,
-  $read_timeout                         = undef,
-  $write_timeout                        = undef,
+  # lint:endignore
+  $metadata_retries             = undef,
+  $wait_for_election            = undef,
+  $background_refresh_frequency = undef,
+  $max_open_reqests             = undef,
+  $dial_timeout                 = undef,
+  $read_timeout                 = undef,
+  $write_timeout                = undef,
+  # lint:ignore:parameter_order
   $topic,
-  $partition                            = undef,
-  $group                                = undef,
-  $default_fetch_size                   = undef,
-  $min_fetch_size                       = undef,
-  $max_message_size                     = undef,
-  $max_wait_time                        = undef,
-  $offset_method                        = undef,
-  $event_buffer_size                    = undef,
+  # lint:endignore
+  $partition                    = undef,
+  $group                        = undef,
+  $default_fetch_size           = undef,
+  $min_fetch_size               = undef,
+  $max_message_size             = undef,
+  $max_wait_time                = undef,
+  $offset_method                = undef,
+  $event_buffer_size            = undef,
 ) {
+  validate_re($ensure, '^(present|absent)$')
   # Common Input Parameters
   if $splitter { validate_string($splitter) }
   if $decoder { validate_string($decoder) }
@@ -118,11 +141,11 @@ define heka::inputs::kafkainput (
   if $min_fetch_size { validate_integer($min_fetch_size) }
   if $max_message_size { validate_integer($max_message_size) }
   if $max_wait_time { validate_integer($max_wait_time) }
-  if $offset_method { validate_string($offset_method) }
+  if $offset_method { validate_re($offset_method, '^(Manual|Newest|Oldest)$') }
   if $event_buffer_size { validate_integer($event_buffer_size) }
 
-  $plugin_name = "kafkainput_${name}"
-  heka::snippet { $plugin_name:
+  $full_name = "kafkainput_${name}"
+  heka::snippet { $full_name:
     ensure  => $ensure,
     content => template("${module_name}/plugin/kafkainput.toml.erb"),
   }
--- manifests/outputs/kafkaoutput.pp
+++ manifests/outputs/kafkaoutput.pp
@@ -2,120 +2,130 @@
 #
 # === Parameters:
 #
-# $ensure::                             This is used to set the status of the config file: present or absent
+# $ensure::                       This is used to set the status of the config file: present or absent
+#                                 Default: present
 #
-### Common Output Parameters::          Check heka::outputs::tcpoutput for the description
+### Common Output Parameters::    Check heka::outputs::tcpoutput for the description
 #
 ### Kafka Output Parameters
 #
-# $message_matcher::                    Boolean expression, when evaluated to true passes the message to the filter for processing.
-#                                       Defaults to matching nothing
+# $id::                           Client ID string.
+#                                 Default is the hostname.
+#                                 Type: string
 #
-# $message_signer::                     The name of the message signer. If specified only messages with this signer are passed to the
-#                                       filter for processing.
+# $addrs::                        List of brokers addresses.
+#                                 Type: []string
 #
-# $encoder::                            Encoder to be used by the output. This should refer to the name of an encoder plugin section that
-#                                       is specified elsewhere in the TOML configuration.
-#                                       Messages can be encoded using the specified encoder by calling the OutputRunner's Encode() method.
+# $metadata_retries::             How many times to retry a metadata request when a partition is in the middle of leader election.
+#                                 Default is 3.
+#                                 Type: int
 #
-# $use_framing::                        Specifies whether or not Heka's Stream Framing should be applied to the binary data returned from
-#                                       the OutputRunner's Encode() method.
+# $wait_for_election::            How long to wait for leader election to finish between retries (in milliseconds).
+#                                 Default is 250.
+#                                 Type: uint32
 #
-# $can_exit::                           Whether or not this plugin can exit without causing Heka to shutdown. Defaults to false.
+# $background_refresh_frequency:: How frequently the client will refresh the cluster metadata in the background (in milliseconds).
+#                                 Default is 600000 (10 minutes). Set to 0 to disable.
+#                                 Type: uint32
 #
-# $id::                                 Client ID string.
-#                                       Default is the hostname.
+# $max_open_reqests::             How many outstanding requests the broker is allowed to have before blocking attempts to send.
+#                                 Default is 4.
+#                                 Type: int
 #
-# $addrs::                              List of brokers addresses.
+# $dial_timeout::                 How long to wait for the initial connection to succeed before timing out and returning an error (in milliseconds).
+#                                 Default is 60000 (1 minute).
+#                                 Type: uint32
 #
-# $metadata_retries::                   How many times to retry a metadata request when a partition is in the middle of leader election.
-#                                       Default is 3.
+# $read_timeout::                 How long to wait for a response before timing out and returning an error (in milliseconds).
+#                                 Default is 60000 (1 minute).
+#                                 Type: uint32
 #
-# $wait_for_election::                  How long to wait for leader election to finish between retries (in milliseconds).
-#                                       Default is 250.
+# $write_timeout::                How long to wait for a transmit to succeed before timing out and returning an error (in milliseconds).
+#                                 Default is 60000 (1 minute).
+#                                 Type: uint32
 #
-# $background_refresh_frequency::       How frequently the client will refresh the cluster metadata in the background (in milliseconds).
-#                                       Default is 600000 (10 minutes). Set to 0 to disable.
+# $partitioner::                  Chooses the partition to send messages to. The valid values are Random, RoundRobin, Hash.
+#                                 Default is Random.
+#                                 Type: string
 #
-# $max_open_reqests::                   How many outstanding requests the broker is allowed to have before blocking attempts to send.
-#                                       Default is 4.
+# $hash_variable::                The message variable used for the Hash partitioner only. The variables are restricted to Type, Logger,
+#                                 Hostname, Payload or any of the message’s dynamic field values. All dynamic field values will be converted to a
+#                                 string representation. Field specifications are the same as with the Message Matcher Syntax e.g. Fields[foo][0][0].
+#                                 Type: string
 #
-# $dial_timeout::                       How long to wait for the initial connection to succeed before timing out and returning an error (in milliseconds).
-#                                       Default is 60000 (1 minute).
+# $topic_variable::               The message variable used as the Kafka topic (cannot be used in conjunction with the ‘topic’ configuration).
+#                                 The variable restrictions are the same as the hash_variable.
+#                                 Type: string
 #
-# $read_timeout::                       How long to wait for a response before timing out and returning an error (in milliseconds).
-#                                       Default is 60000 (1 minute).
+# $topic::                        A static Kafka topic (cannot be used in conjunction with the ‘topic_variable’ configuration).
+#                                 Type: string
 #
-# $write_timeout::                      How long to wait for a transmit to succeed before timing out and returning an error (in milliseconds).
-#                                       Default is 60000 (1 minute).
+# $required_acks::                The level of acknowledgement reliability needed from the broker.
+#                                 The valid values are NoResponse, WaitForLocal, WaitForAll.
+#                                 Default is WaitForLocal.
+#                                 Type: string
 #
-# $partitioner::                        Chooses the partition to send messages to. The valid values are Random, RoundRobin, Hash.
-#                                       Default is Random.
+# $timeout::                      The maximum duration the broker will wait for the receipt of the number of RequiredAcks (in milliseconds).
+#                                 This is only relevant when RequiredAcks is set to WaitForAll. Default is no timeout.
+#                                 Type: uint32
 #
-# $hash_variable::                      he message variable used for the Hash partitioner only. The variables are restricted to Type, Logger,
-#                                       Hostname, Payload or any of the message’s dynamic field values. All dynamic field values will be converted to a
-#                                       string representation. Field specifications are the same as with the Message Matcher Syntax e.g. Fields[foo][0][0].
+# $compression_codec::            The type of compression to use on messages. The valid values are None, GZIP, Snappy.
+#                                 Default is None.
+#                                 Type: string
 #
-# $topic_variable::                     The message variable used as the Kafka topic (cannot be used in conjunction with the ‘topic’ configuration).
-#                                       The variable restrictions are the same as the hash_variable.
+# $max_buffer_time::              The maximum duration to buffer messages before triggering a flush to the broker (in milliseconds).
+#                                 Default is 1.
+#                                 Type: uint32
 #
-# $topic::                              A static Kafka topic (cannot be used in conjunction with the ‘topic_variable’ configuration).
+# $max_buffered_bytes::           The threshold number of bytes buffered before triggering a flush to the broker.
+#                                 Default is 1.
+#                                 Type: uint32
 #
-# $required_acks::                      The level of acknowledgement reliability needed from the broker.
-#                                       The valid values are NoResponse, WaitForLocal, WaitForAll.
-#                                       Default is WaitForLocal.
+# $back_pressure_threshold_bytes:: The maximum number of bytes allowed to accumulate in the buffer before back-pressure is applied to QueueMessage.
+#                                 Without this, queueing messages too fast will cause the producer to construct requests larger than
+#                                 the MaxRequestSize (100 MiB).
+#                                 Default is 50 * 1024 * 1024 (50 MiB), cannot be more than (MaxRequestSize - 10 KiB).
+#                                 Type: uint32
 #
-# $timeout::                            The maximum duration the broker will wait for the receipt of the number of RequiredAcks (in milliseconds).
-#                                       This is only relevant when RequiredAcks is set to WaitForAll. Default is no timeout.
-#
-# $compression_codec::                  The type of compression to use on messages. The valid values are None, GZIP, Snappy.
-#                                       Default is None.
-#
-# $max_buffer_time::                    The maximum duration to buffer messages before triggering a flush to the broker (in milliseconds).
-#                                       Default is 1.
-#
-# $max_buffered_bytes::                 The threshold number of bytes buffered before triggering a flush to the broker.
-#                                       Default is 1.
-#
-# $back_pressure_threshold_bytes::      The maximum number of bytes allowed to accumulate in the buffer before back-pressure is applied to QueueMessage.
-#                                       Without this, queueing messages too fast will cause the producer to construct requests larger than
-#                                       the MaxRequestSize (100 MiB). Default is 50 * 1024 * 1024 (50 MiB), cannot be more than (MaxRequestSize - 10 KiB).
-#
-
 define heka::outputs::kafkaoutput (
-  $ensure                       = 'present',
+  $ensure                        = 'present',
   # Common Output Parameters
   $message_matcher,
-  $message_signer                       = undef,
-  $encoder                              = 'ProtobufEncoder',
-  $use_framing                          = undef,
-  $can_exit                             = undef,
+  $message_signer                = undef,
+  $encoder                       = 'ProtobufEncoder',
+  $use_framing                   = undef,
+  $can_exit                      = undef,
   # Buffering
-  $max_file_size                        = undef,
-  $max_buffer_size                      = undef,
-  $full_action                          = undef,
-  $cursor_update_count                  = undef,
+  $max_file_size                 = undef,
+  $max_buffer_size               = undef,
+  $full_action                   = undef,
+  $cursor_update_count           = undef,
   # Kafka Output
-  $id                                   = undef,
+  $id                            = undef,
+  # lint:ignore:parameter_order
   $addrs,
-  $metadata_retries                     = undef,
-  $wait_for_election                    = undef,
-  $background_refresh_frequency         = undef,
-  $max_open_reqests                     = undef,
-  $dial_timeout                         = undef,
-  $read_timeout                         = undef,
-  $write_timeout                        = undef,
-  $partitioner                          = undef,
-  $hash_variable                        = undef,
-  $topic_variable                       = undef,
+  # lint:endignore
+  $metadata_retries              = undef,
+  $wait_for_election             = undef,
+  $background_refresh_frequency  = undef,
+  $max_open_reqests              = undef,
+  $dial_timeout                  = undef,
+  $read_timeout                  = undef,
+  $write_timeout                 = undef,
+  $partitioner                   = undef,
+  $hash_variable                 = undef,
+  $topic_variable                = undef,
+  # lint:ignore:parameter_order
   $topic,
-  $required_acks                        = undef,
-  $timeout                              = undef,
-  $compression_codec                    = undef,
-  $max_buffer_time                      = undef,
-  $max_buffered_bytes                   = undef,
-  $back_pressure_threshold_bytes        = undef,
+  # lint:endignore
+  $required_acks                 = undef,
+  $timeout                       = undef,
+  $compression_codec             = undef,
+  $max_buffer_time               = undef,
+  $max_buffered_bytes            = undef,
+  $back_pressure_threshold_bytes = undef,
 ) {
+  validate_re($ensure, '^(present|absent)$')
   # Common Output Parameters
   if $message_matcher { validate_string($message_matcher) }
   if $message_signer { validate_string($message_signer) }
@@ -148,8 +158,8 @@ define heka::outputs::kafkaoutput (
   if $max_buffered_bytes { validate_integer($max_buffered_bytes) }
   if $back_pressure_threshold_bytes { validate_integer($back_pressure_threshold_bytes) }
 
-  $plugin_name = "kafkaoutput_${name}"
-  heka::snippet { $plugin_name:
+  $full_name = "kafkaoutput_${name}"
+  heka::snippet { $full_name:
     ensure  => $ensure,
     content => template("${module_name}/plugin/kafkaoutput.toml.erb"),
   }
