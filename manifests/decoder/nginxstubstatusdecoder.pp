2add4a6bb63d1ed295987a6b1008aa702372e2e3
Cristian Falcas <cristi.falcas@gmail.com>
2016-02-22 17:43:31 +0200

    add nginxstubstatus decoder

---
+++ manifests/decoder/nginxstubstatusdecoder.pp
@@ -0,0 +1,55 @@
+# Parses a payload containing the output of nginx's stub status module: http://nginx.org/en/docs/http/ngx_http_stub_status_module.html
+# It uses the sandboxdecoder
+#
+# === Parameters:
+#
+# $ensure::                       This is used to set the status of the config file: present or absent
+#                                 Default: present
+#
+### Check heka::decoder::sandboxdecoder for common parameters
+#
+### NginxStubStatus Parameters
+#
+# $type::                         Sets the message 'Type' header to the specified value
+#                                 Type: string
+#
+# $payload_keep::                 Always preserve the original log line in the message payload.
+#                                 Type: bool
+#
+define heka::decoder::nginxstubstatusdecoder (
+  $ensure            = 'present',
+  # Common Sandbox Parameters
+  $preserve_data     = undef,
+  $memory_limit      = undef,
+  $instruction_limit = undef,
+  $output_limit      = undef,
+  $module_directory  = undef,
+  # NginxStubStatus Parameters
+  $type              = $name,
+  $payload_keep      = false,
+) {
+  validate_re($ensure, '^(present|absent)$')
+  # Common Sandbox Parameters
+  if $preserve_data { validate_bool($preserve_data) }
+  if $memory_limit { validate_integer($memory_limit) }
+  if $instruction_limit { validate_integer($instruction_limit) }
+  if $output_limit { validate_integer($output_limit) }
+  if $module_directory { validate_string($module_directory) }
+  # NginxStubStatus Parameters
+  validate_string($type)
+  validate_bool($payload_keep)
+
+  $sandbox_type = 'SandboxDecoder'
+  $script_type = 'lua'
+  $filename = 'lua_decoders/nginx_stub_status.lua'
+  $config = {
+    'type'         => $type,
+    'payload_keep' => $payload_keep,
+  }
+
+  $full_name = "nginxstubstatusdecoder_${name}"
+  heka::snippet { $full_name:
+    ensure  => $ensure,
+    content => template("${module_name}/sandbox.toml.erb"),
+  }
+}
