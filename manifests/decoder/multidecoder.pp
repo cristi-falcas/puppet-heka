642edd2a12a412f46207ed11e62abcb18440cefd
Cristian Falcas <cristi.falcas@gmail.com>
2016-02-22 17:25:30 +0200

    add checks

--- manifests/decoder/multidecoder.pp
+++ manifests/decoder/multidecoder.pp
@@ -5,25 +5,42 @@
 #
 # === Parameters:
 #
-# $subs::                          An ordered list of subdecoders to which the MultiDecoder will delegate.
-#                                  Each item in the list should specify another decoder configuration section
-#                                  by section name. Must contain at least one entry.
+# $ensure::                       This is used to set the status of the config file: present or absent
+#                                 Default: present
 #
-# $cascade_strategy::              Specifies behavior the MultiDecoder should exhibit with regard to cascading
-#                                  through the listed decoders. Supports only two valid values: "first-wins" and "all".
-#                                  With "first-wins", each decoder will be tried in turn until there is a successful
-#                                  decoding, after which decoding will be stopped. With "all", all listed decoders
-#                                  will be applied whether or not they succeed. In each case, decoding will only be
-#                                  considered to have failed if none of the sub-decoders succeed.
+# $subs::                         An ordered list of subdecoders to which the MultiDecoder will delegate.
+#                                 Each item in the list should specify another decoder configuration section
+#                                 by section name. Must contain at least one entry.
+#                                 Type: string
 #
-# $log_sub_errors::                If true, the DecoderRunner will log the errors returned whenever a delegate
-#                                  decoder fails to decode a message. Defaults to false.
+# $log_sub_errors::               If true, the DecoderRunner will log the errors returned whenever a delegate
+#                                 decoder fails to decode a message.
+#                                 Defaults to false.
+#                                 Type: bool
 #
-
+# $cascade_strategy::             Specifies behavior the MultiDecoder should exhibit with regard to cascading
+#                                 through the listed decoders. Supports only two valid values: "first-wins" and "all".
+#                                 With "first-wins", each decoder will be tried in turn until there is a successful
+#                                 decoding, after which decoding will be stopped. With "all", all listed decoders
+#                                 will be applied whether or not they succeed. In each case, decoding will only be
+#                                 considered to have failed if none of the sub-decoders succeed.
+#                                 Type: string
+#
 define heka::decoder::multidecoder (
+  $ensure           = 'present',
+  # MultiDecoder Parameters
   $subs,
+  $log_sub_errors   = false,
   $cascade_strategy = undef,
-  $log_sub_errors = false,
 ){
-  heka::snippet { $name: content => template("${module_name}/decoder/multidecoder.toml.erb"), }
+  validate_re($ensure, '^(present|absent)$')
+  validate_string($subs)
+  validate_bool($log_sub_errors)
+  if $cascade_strategy { validate_string($cascade_strategy) }
+
+  $full_name = "multidecoder_${name}"
+  heka::snippet { $full_name:
+    ensure  => $ensure,
+    content => template("${module_name}/decoder/multidecoder.toml.erb"),
+  }
 }
